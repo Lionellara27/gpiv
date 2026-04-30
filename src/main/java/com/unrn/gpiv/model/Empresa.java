bfe35229e892e5cf0c5fdd4933d899010909ab57
MarianoGambaro <martinezgambaromariano@gmail.com>
2026-04-30 12:12:23 -0600

    agregue los archivos de vaadin frontend\generated al git ignore e hice clase Lote y Empresa

--- src/main/java/com/unrn/gpiv/model/Empresa.java
+++ src/main/java/com/unrn/gpiv/model/Empresa.java
@@ -1,4 +1,12 @@
 package com.unrn.gpiv.model;
 
 public class Empresa {
+    private int id;
+    private String nombreEmpresa;
+    private ProyectoProductivo proyecto;
+    private String cuit;
+    private Lote lote;
+    private int  cantidadEmpleados;
+    private boolean titulada;
+
 }
--- src/main/java/com/unrn/gpiv/model/Lote.java
+++ src/main/java/com/unrn/gpiv/model/Lote.java
@@ -1,4 +1,53 @@
 package com.unrn.gpiv.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lote {
+    private static final boolean DISPONIBLE = true;
+    private static final boolean OCUPADO = false;
+
+    private int id;
+    private String numeroDeLote;
+    private double metrosCuadrados;
+    private List<Servicio> servicios;
+    private boolean estadoActual;
+
+    public Lote(String numeroDeLote, double metrosCuadrados) {
+        this.numeroDeLote = numeroDeLote;
+        this.metrosCuadrados = metrosCuadrados;
+        this.servicios = new ArrayList<>();
+        this.estadoActual = DISPONIBLE;
+    }
+
+    public void agregarServicio(Servicio servicio){
+        servicios.add(servicio);
+    }
+
+    private boolean validarNumeroDeLoteNoExistente(){
+        return true;
+    }
+
+    private boolean validarMetrosCuadrados(){
+        return metrosCuadrados > 0;
+    }
+    public boolean estado(){
+        return estadoActual;
+    }
+
+    private void cambiarEstadoADisponible(){
+        this.estadoActual = DISPONIBLE;
+    }
+    private void cambiarEstadoAOcupado(){
+        this.estadoActual = OCUPADO;
+    }
+    public String numeroDeLote(){
+        return numeroDeLote;
+    }
+    public int id(){
+        return id;
+    }
+
 }
+
+
